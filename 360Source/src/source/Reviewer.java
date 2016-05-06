9f8275314591c85767b2822dccdc590429cec324
natez <natez@10.65.82.173>
2016-05-05 19:59:51 -0700

    Merged with Anh's code.

---
+++ 360Source/src/source/Reviewer.java
@@ -0,0 +1,65 @@
+package source;
+import java.io.Serializable;
+
+/**
+ * Reviewer class. Defines the role of a Reviewer.
+ * @author Anh Tran
+ * @version 05/06/2016
+ */
+public class Reviewer implements Serializable{
+
+    //For Serialization only
+    private static final long serialVersionUID = -1242844363061411863L;
+    /**
+     * Username of Reviewer
+     */
+    private String userName;
+
+    /**
+     * Constructor. Instantiates class with username and roles.
+     * @param newUserName username of Reviewer
+     */
+    public Reviewer(String newUserName) {
+        userName = newUserName;
+    }
+
+    /**
+     * Submits review of Reviewer
+     * @param review The Reviewer's review of a Manuscript
+     * @return Returns review submitted. Will be stored and handled in main
+     * @exception NullPointerException if no review exists or is found (null)
+     */
+    public String submitReview(String review) {
+        if(review == null) {
+            throw new NullPointerException("No review found");
+        }
+        return review;
+    }
+
+    /**
+     * Resubmits a new  or edited review
+     * @param review The Reviewer's new or edited review of a Manuscript
+     * @return Returns review submitted. Will be stored and handled in main
+     */
+    public String changeReview(String review) {
+        return submitReview(review);
+    }
+
+    /**
+     * Unsubmits a review belonging to Reviewer
+     * @return Returns null review. Will be stored and handled in main
+     */
+    public String unSubmitReview() {
+        return null;
+    }
+
+    /**
+     * Getter. Gets username of Reviewer.
+     * @return The username of Reviewer
+     */
+    public String getUsername() {
+        return userName;
+    }
+
+
+}
---
+++ 360Source/src/source/SubprogramChair.java
@@ -0,0 +1,66 @@
+package source;
+import java.io.Serializable;
+
+/**
+ * @author Anh Tran
+ * @version 05052016 //Date Written
+ */
+public class SubprogramChair implements Serializable{
+
+    //For Serialization only
+    private static final long serialVersionUID = 3523359274963269247L;
+    /**
+     * Username of Subprogram Chair
+     */
+    String userName;
+
+    /**
+     * Constructor. Creates a subprogram chair role.
+     * @param newUserName Username of Subprogram Chair
+     */
+    public SubprogramChair(String newUserName) {
+        userName = newUserName;
+    }
+
+    /**
+     * Assigns a reviewer to a Manuscript. No assignment if Manuscript is not assigned to Subprogram Chair
+     * @param paper Manuscript that needs a reviewer
+     * @param reviewer Reviewer to review the Manuscript
+     * @exception NullPointerException if Manuscript does not exist (null)
+     * @exception NullPointerException if Reviewer does not exist (null)
+     */
+    public void assignReviewer(Manuscript paper, Reviewer reviewer) {
+        if(paper == null) {
+            throw new NullPointerException();
+        }
+        if(reviewer == null) {
+            throw new NullPointerException();
+        }
+        if(!paper.getSPCsUsername().equals(userName)) {
+            //Incorrect SPC for paper.
+            return;
+        }
+        paper.addReviewer(reviewer);
+    }
+
+    /**
+     * Submits recommendation of Subprogram Chair
+     * @param recommendation String that contains the Subprogram Chair's recommendation
+     * @return Object of the Recommendation class to be handled by main
+     * @exception  NullPointerException if recommendation is null
+     */
+    public Recommendation submitRecommendation(String recommendation) {
+        if(recommendation == null) {
+            throw new NullPointerException("No recommendation found");
+        }
+        return new Recommendation(this, recommendation);
+    }
+
+    /**
+     * Getter. Gets username of Subprogram Chair
+     * @return Returns username
+     */
+    public String getUserName() {
+        return userName;
+    }
+}
