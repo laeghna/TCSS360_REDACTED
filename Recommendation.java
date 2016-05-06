dae98206da0337c95f61f3c58b4bd5c560607c1e
Andy Tran <lordandytran@users.noreply.github.com>
2016-05-05 19:37:53 -0700

    Uploaded java classes.
    Added Reviewer, Review, Recommendation, and Subprogram Chair. Should be finished. Needs to be merged with other classes and debugged. JavaDocs finished for all classes.

---
+++ Recommendation.java
@@ -0,0 +1,74 @@
+import java.io.Serializable;
+
+/**
+ * Recommendation class. Stores Subprogram Chair and their recommendation.
+ * @author Anh Tran
+ * @version 05/06/2016
+ */
+public class Recommendation implements Serializable{
+
+    //For Serialization only
+    private static final long serialVersionUID = -6783961909189765303L;
+    /**
+     * Subprogram Chair that made recommendation
+     */
+    private SubprogramChair SPC;
+    /**
+     * Recommendation of Subprogram Chair
+     */
+    private String recommendation;
+
+    /**
+     * Constructor. Creates recommendation class
+     * @param spc Subprogram Chair that makes recommendation
+     * @param rec Recommendation of Subprogram Chair
+     * @exception NullPointerException if spc (Subprogram Chair) is null
+     */
+    public Recommendation(SubprogramChair spc, String rec) {
+        if(spc == null) {
+            throw new NullPointerException("SPC does not exist");
+        }
+        SPC = spc;
+        recommendation = rec;
+    }
+
+    /**
+     * Getter. Gets Subprogram Chair
+     * @return Returns the Subprogram Chair
+     */
+    public SubprogramChair getSPC() {
+        return SPC;
+    }
+
+    /**
+     * Getter. Gets the submitted recommendation
+     * @return Returns the Subprogram Chair's recommendation
+     */
+    public String getRecommendation() {
+        return recommendation;
+    }
+
+    /**
+     * Setter. Sets the Subprogram Chair.
+     * @param newSPC The Subprogram Chair that makes the recommendation
+     * @exception NullPointerException if Subprogram Chair does not exist (null)
+     */
+    public void setSPC(SubprogramChair newSPC) {
+        if(newSPC == null) {
+            throw new NullPointerException("No Subprogram Chair found");
+        }
+        SPC = newSPC;
+    }
+
+    /**
+     * Setter. Sets the recommendation
+     * @param newRec The submitted recommendation
+     * @exception NullPointerException if recommendation does not exist (null)
+     */
+    public void setRecommendation(String newRec) {
+        if(newRec == null) {
+            throw new NullPointerException("No recommendation found");
+        }
+        recommendation = newRec;
+    }
+}
---
+++ Review.java
@@ -0,0 +1,100 @@
+import java.io.Serializable;
+
+/**
+ * Review class. Stores Reviewer and their review of a Manuscript
+ * @author Anh Tran
+ * @version 05/06/2016
+ */
+public class Review implements Serializable{
+
+    //For Serialization only
+    private static final long serialVersionUID = 8767635707411068900L;
+    /**
+     * Reviewer assigned to manuscript
+     */
+    private Reviewer reviewer;
+    /**
+     * Reviewer submission (if any)
+     */
+    private String reviewSubmission;
+
+    /**
+     * Default Constructor. No reviewer or review assigned. All fields to null.
+     */
+    public Review() {
+        reviewSubmission = null;
+        reviewer = null;
+    }
+
+    /**
+     * Constructor. Assigns reviewer only.
+     * @param newReviewer reviewer assigned to manuscript
+     */
+    public Review(Reviewer newReviewer) {
+        if(newReviewer == null) {
+            throw new NullPointerException("No such reviewer");
+        }
+        setReviewer(newReviewer);
+        reviewSubmission = null;
+    }
+
+    /**
+     * Full constructor. Assigns both a reviewer and their review.
+     * @param newReviewer reviewer assigned to manuscript
+     * @param review review submitted by reviewer
+     */
+    public Review(Reviewer newReviewer, String review) {
+        if(newReviewer == null) {
+            throw new NullPointerException("No such reviewer");
+        }
+        if(review == null) {
+            throw new NullPointerException("No review found.");
+        }
+        setReviewer(newReviewer);
+        setReviewSubmission(review);
+    }
+
+    /**
+     * Getter. Returns reviewer.
+     * @return reviewer that was assigned to the manuscript
+     */
+    public Reviewer getReviewer() {
+        return reviewer;
+    }
+
+    /**
+     * Getter. Returns submission.
+     * @return Returns submission of reviewer. Null if none exists.
+     */
+    public String getReviewSubmission() {
+        return reviewSubmission;
+    }
+
+    /**
+     * Setter. Sets reviewer to manuscript
+     * @param newReviewer
+     * @exception NullPointerException if reviewer does not exist (null)
+     */
+    private void setReviewer(Reviewer newReviewer) {
+        if(reviewer == null) {
+            throw new NullPointerException("No such reviewer");
+        }
+        reviewer = newReviewer;
+    }
+
+    /**
+     * Setter. Sets new review submission
+     * @param review review that is to be added
+     * @exception IllegalArgumentException if no reviewer is assigned
+     * @exception NullPointerException if review does not exist or not found
+     */
+    private void setReviewSubmission(String review) {
+        if(reviewer == null) {
+            throw new IllegalArgumentException("No reviewer assigned. Cannot add submission");
+        }
+        if(review == null) {
+            throw new NullPointerException("No review found.");
+        }
+        reviewSubmission = review;
+    }
+}
