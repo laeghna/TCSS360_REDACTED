d8d3d03a71a7cdff5878b186ba777e85eaa39583
natez <natez@DESKTOP-C1DOCI1>
2016-05-07 17:34:37 -0700

    Pushed most recent code to master

---
+++ 360Source/src/tests/ConferenceTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConferenceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ 360Source/src/tests/ManuscriptTests.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ManuscriptTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
